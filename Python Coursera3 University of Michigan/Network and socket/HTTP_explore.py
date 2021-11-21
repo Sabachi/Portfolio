89e142f661ecd8a63c3e11880d4b7f236cebf3da
Sabachi <kinsukmanisinha@gmail.com>
2021-11-21 21:34:16 +0100

    Request response cycle

--- Python Coursera3 University of Michigan/Network and socket/HTTP_explore.py
+++ Python Coursera3 University of Michigan/Network and socket/HTTP_explore.py
@@ -1,18 +1,23 @@
 #Exploring the HyperText Transport Protocol
-#You are to retrieve the following document using the HTTP protocol in a way
-#that you can examine the HTTP Response headers.
+# Request response cycle
 
 import socket
 
+# Returns an object which creates a connection across the internet
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Establish a connection to a port on a domain
 mysock.connect(('data.pr4e.org', 80))
+
+# The rule of internet protocol is that first you send something, with the send
+# method
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
+    data = mysock.recv(512) # Receives upto 512 characters
+    if len(data) < 1: # if no data then end the file, this is the stop condition
         break
-    print(data.decode(),end='')
+    print(data.decode(),end='') # decode the data from utf-8 to unicode
 
 mysock.close()
