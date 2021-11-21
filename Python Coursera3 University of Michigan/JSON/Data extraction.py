f8b30246f6fa0c9931be22718b31cd0f7af1c853
Sabachi <kinsukmanisinha@gmail.com>
2021-11-21 21:41:40 +0100

    JSON counter

--- Python Coursera3 University of Michigan/JSON/Data extraction.py
+++ Python Coursera3 University of Michigan/JSON/Data extraction.py
@@ -8,16 +8,21 @@ import urllib.request, urllib.parse, urllib.error
 import json
 
 url_raw = input('Enter - ')
+
+# Gives a handle into which the document has been read
 data = urllib.request.urlopen(url_raw).read()
 number = list()
 
+# Parse the JSON and store it as dictionary
 info = json.loads(data)
+
+# Dumps the data in json format, to receive readable data
 print(json.dumps(info, indent = 4))
 print('User count:', len(info))
-items = info["comments"]
+items = info["comments"] # extract value for the key comments
 
 for item in items:
-    num = item["count"]
+    num = item["count"] # extract value for the key count
     print(num)
     number.append(num)
 
