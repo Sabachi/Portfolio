82981fa17746565f795e6a75dbdecc45a319a8f2
Sabachi <kinsukmanisinha@gmail.com>
2021-11-21 21:01:52 +0100

    XML assignment

--- Python Coursera3 University of Michigan/Web services and XML/Compute sum_I.py
+++ Python Coursera3 University of Michigan/Web services and XML/Compute sum_I.py
@@ -3,14 +3,21 @@
 #urllib and then parse and extract the comment counts from the XML data,
 #compute the sum of the numbers in the file.
 
+
 import xml.etree.ElementTree as ET
 import urllib.request, urllib.parse, urllib.error
 
 
 data = "http://py4e-data.dr-chuck.net/comments_1386938.xml"
+
+#Returns a handle to read the data
 uh = urllib.request.urlopen(data).read()
+
+# ET object which has parsed XML data.
 tree = ET.fromstring(uh)
 
+# Use the method findall to gather all the çomment tags,which are children
+# tags of comments.
 First = tree.findall('comments/comment')
 number = list()
 
