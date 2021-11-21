0a39f0b102d7b2780ed1c6589dbedde0121575cb
Sabachi <kinsukmanisinha@gmail.com>
2021-11-21 21:11:07 +0100

    Scrape numbers Bsoup

--- Python Coursera3 University of Michigan/Programs that surf the web/Scrape numbers_ Beautifulsoup.py
+++ Python Coursera3 University of Michigan/Programs that surf the web/Scrape numbers_ Beautifulsoup.py
@@ -9,14 +9,18 @@ from bs4 import BeautifulSoup
 import ssl
 
 
-
 # Ignore SSL certificate errors
+# These codes will be useful if it is a http page
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
+
+# Returns a file handle into which the html page has been read
 html = urlopen(url, context=ctx).read()
+
+# Parse the html page and store into the beautiful soup object
 soup = BeautifulSoup(html, "html.parser")
 
 results = list()
@@ -25,9 +29,10 @@ results = list()
 tags = soup('span')
 #print("tags",tags)
 
+# Example of a tag from the assignment: <span class="comments">2</span>
 for tag in tags:
     print("new line", tag)
-    tag_1 = str(tag)
+    tag_1 = str(tag) # From beautiful element to string 
     result = tag_1.split()
     result_1 = result[1].split('>')
     result_2 = result_1[1].split('<')
